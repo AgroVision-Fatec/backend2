699a7e9c4b662037925faad40ef25591d7ebb017
LucasMedici <lucasrpmedici@gmail.com>
2024-05-13 18:14:19 -0300

    liberando cors e requisicoes da mesma rede

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, jsonify
 import os
 from contabilizar_pragas import script_pragas
+from flask_cors import CORS
 
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/processar-imagem', methods=['POST'])
 def processar_imagem():
+    print('batendo na rota')
     if 'imagem' not in request.files:
         return jsonify({'error': 'No image provided'}), 400
     
@@ -17,7 +20,7 @@ def processar_imagem():
     resultado = script_pragas()
   
 
-    os.remove('IA/imgs/imagem.jpg')
+    # os.remove('IA/imgs/imagem.jpg')
     return jsonify({'resultado': resultado})
 
 
@@ -27,4 +30,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
